b58f363d38a6b7d49063318ae632f271f451def6
evelyntecinternet <142834269+evelyntecinternet@users.noreply.github.com>
2023-11-30 15:18:09 -0300

    Create Helloword.py

---
+++ Helloword.py
@@ -0,0 +1,9 @@
+print("Hello Word !") #mostra na tela
+mensagem = "ola, tudo bem?"
+print(mensagem)
+print(mensagem[1])
+print(mensagem[0:4])
+print(mensagem[0:11])
+print(mensagem[-1])
+print(mensagem[10])
+print(mensagem[4::])# mostra da quarta posição até o fim
